869b3f20f10d62f0c8fd42379dea53f23931b803
MoayadGergawi <78973992+MoayadGergawi@users.noreply.github.com>
2021-06-12 22:22:54 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Person()
+        {
+            return View();
+        }
+
+        public IActionResult Count()
+        {
+            return View();
+        }
+
+        public IActionResult DisplayPerson()
+        {
+            return View();
+        }
+
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        public int FirstName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(1000)]
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(1000)]
+        public string Age
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(1000)]
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(1000)]
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(1000)]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+    }
+}
